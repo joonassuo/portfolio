190262aabc7ed66d4d53a998f6cd932d46f70639
Joonas <joonas.suonpera@gmail.com>
2020-06-10 12:17:55 +0300

    styling of about page

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -10,11 +10,87 @@ const About: React.FC<Props> = ({ slideClass, toggleClass }) => {
   return (
     <div id="about-container" className={slideClass}>
       <img
-        src="/arrow.png"
+        src="/icons/arrow.png"
         alt="back-arrow"
         id="about-back"
         onClick={() => toggleClass("slide-out")}
       />
+      <div id="quote">
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam minus
+        veniam nemo possimus unde? Illum a expedita distinctio minus tempore."
+      </div>
+      <div id="languages-label-container">
+        <div id="languages-label">languages</div>
+        <div className="strikethrough"></div>
+      </div>
+      <div className="languages-container">
+        <img
+          src="/langs/react.png"
+          alt="react"
+          id="react-icon"
+          className="languages-icon"
+        />
+        <img
+          src="/langs/redux.png"
+          alt="redux"
+          id="redux-icon"
+          className="languages-icon"
+        />
+        <img
+          src="/langs/javascript.png"
+          alt="javascript"
+          id="javascript-icon"
+          className="languages-icon"
+        />
+        <img
+          src="/langs/typescript.png"
+          alt="typescript"
+          id="typescript-icon"
+          className="languages-icon"
+        />
+        <img
+          src="/langs/c.png"
+          alt="c"
+          id="c-icon"
+          className="languages-icon"
+        />
+        <img
+          src="/langs/node.png"
+          alt="node"
+          id="node-icon"
+          className="languages-icon"
+        />
+      </div>
+      <div id="other-tech-label-container">
+        <div id="other-tech-label">other tech</div>
+        <div className="strikethrough"></div>
+      </div>
+      <div className="tech-container">
+        <img
+          src="/tech/aws.png"
+          alt="aws"
+          id="aws-icon"
+          className="tech-icon"
+        />
+        <img
+          src="/tech/mongo.png"
+          alt="mongo"
+          id="mongo-icon"
+          className="tech-icon"
+        />
+        <img
+          src="/tech/docker.png"
+          alt="docker"
+          id="docker-icon"
+          className="tech-icon"
+        />
+        <img
+          src="/tech/linux.png"
+          alt="linux"
+          id="linux-icon"
+          className="tech-icon"
+        />
+      </div>
     </div>
   );
 };
--- src/Components/Frontpage/Frontpage.tsx
+++ src/Components/Frontpage/Frontpage.tsx
@@ -13,19 +13,19 @@ const Frontpage: React.FC<Props> = ({ toggleAboutClass }) => {
         <div id="legend">Full-Stack Developer</div>
         <div className="links-container">
           <img
-            src="/github.png"
+            src="/social/github.png"
             alt="github"
             className="social-link"
             id="github-icon"
           />
           <img
-            src="/linkedin.png"
+            src="/social/linkedin.png"
             alt="linkedin"
             className="social-link"
             id="linkedin-icon"
           />
           <img
-            src="/email.png"
+            src="/social/email.png"
             alt="email"
             className="social-link"
             id="email-icon"
@@ -41,7 +41,7 @@ const Frontpage: React.FC<Props> = ({ toggleAboutClass }) => {
         About
       </div>
       <div id="contact">Contact</div>
-      <img src="/arrow.png" alt="arrow" id="scroll-arrow" />
+      <img src="/icons/arrow.png" alt="arrow" id="scroll-arrow" />
     </div>
   );
 };
