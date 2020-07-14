9c436406272cfb12c0972f3e811f38299390332b
Joonas <joonas.suonpera@gmail.com>
2020-07-14 17:03:32 +0300

    added frontpage logo. pushing before making portfolio item changes according to new design

--- src/Components/Frontpage/Frontpage.tsx
+++ src/Components/Frontpage/Frontpage.tsx
@@ -15,29 +15,26 @@ const Frontpage: React.FC<Props> = ({ toggleAboutClass, portfolioIndex }) => {
           : "frontpage-container"
       }
     >
-      <div className="title-container">
-        <div id="name">JOONAS SUONPERÄ</div>
-        <div id="legend">Full-Stack Developer</div>
-        <div className="links-container">
-          <img
-            src="/social/github.png"
-            alt="github"
-            className="social-link"
-            id="github-icon"
-          />
-          <img
-            src="/social/linkedin.png"
-            alt="linkedin"
-            className="social-link"
-            id="linkedin-icon"
-          />
-          <img
-            src="/social/email.png"
-            alt="email"
-            className="social-link"
-            id="email-icon"
-          />
-        </div>
+      <img id="frontpage-logo" src="/frontpage_logo.png" alt="logo" />
+      <div className="links-container">
+        <img
+          src="/social/github.png"
+          alt="github"
+          className="social-link"
+          id="github-icon"
+        />
+        <img
+          src="/social/linkedin.png"
+          alt="linkedin"
+          className="social-link"
+          id="linkedin-icon"
+        />
+        <img
+          src="/social/email.png"
+          alt="email"
+          className="social-link"
+          id="email-icon"
+        />
       </div>
       <div
         id="about"
